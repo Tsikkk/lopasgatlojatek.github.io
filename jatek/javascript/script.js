d2e4fc18fc60dd61ca2f8bca5ec094fe4cca97ed
Kántor Krisztián <kantor.krisztian@AD.LOCAL>
2024-01-08 14:26:30 +0100

    asd

---
+++ jatek/javascript/script.js
@@ -0,0 +1,33 @@
+const tabla=document.getElementById("tabla")
+
+var szam=null
+var palya=[["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""],["", "", "","", "", "","", "", ""]]
+
+
+function one(){
+    szam=1
+}
+function two(){
+    szam=2
+}
+function three(){
+    szam=3
+}
+function four(){
+    szam=4
+}
+function five(){
+    szam=5
+}
+function six(){
+    szam=6
+}
+function seven(){
+    szam=7
+}
+function eight(){
+    szam=8
+}
+function nine(){
+    szam=9
+}
